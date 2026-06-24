a3e584af8e27bc227d9115933cf6834887ca5e9e
rifkingrha <rifkingrha@gmail.com>
2026-06-24 13:57:11 +0700

    fixing config

--- no_analisa_rest/config/database.js
+++ no_analisa_rest/config/database.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('db_no_analisis', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  timezone: '+07:00'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    timezone: '+07:00'
+  }
+);
 
 module.exports = sequelize;
