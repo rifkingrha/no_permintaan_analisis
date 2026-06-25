62c2d0ea22e869e586571b52646a3a3fdd30275a
rifkingrha <rifkingrha@gmail.com>
2026-06-25 09:42:38 +0700

    fixing chromium for puppeteer

--- no_analisa_rest/app/controllers/list_no_analisis/generateReport.js
+++ no_analisa_rest/app/controllers/list_no_analisis/generateReport.js
@@ -1,7 +1,16 @@
 const puppeteer = require('puppeteer')
 
 module.exports = async (html) => {
-  const browser = await puppeteer.launch({headless: 'true'})
+  const browser = await puppeteer.launch({
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH, 
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu'
+    ],
+    headless: true
+  })
   const page = await browser.newPage()
 
   await page.setContent(html, {
